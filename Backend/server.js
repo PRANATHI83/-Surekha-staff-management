8fa692ca8bb7ef5bdcc1b5989582765b179b0327
PRANATHI83 <apranu8@gmail.com>
2025-06-23 09:49:54 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3014;
+const port = process.env.PORT || 3015;
 
 // Middleware
 app.use(cors());
