add0e9c5acb590b072f39ff0153c4292a9b7a04e
PRANATHI83 <apranu8@gmail.com>
2025-06-23 10:56:55 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -173,6 +173,6 @@ app.delete('/api/employees/:empId', async (req, res) => {
 });
 
 // Start server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
 });
